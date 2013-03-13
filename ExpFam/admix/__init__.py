0db647c568a6d4a383c6f8f6d7d257930085f8ad
Mike Hughes <mhughes@cs.brown.edu>
2013-03-13 11:34:38 -0400

    Clean up successful

--- ExpFam/admix/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The :mod:`admix` module provides variational approximations to 
-  admixture models (in the style of Latent Dirichlet Allocation).
-"""
-
-from .AdmixModel import AdmixModel
-from .HDPAdmixModel import HDPAdmixModel
